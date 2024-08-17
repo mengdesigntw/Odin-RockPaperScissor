3aa9ede58f1633705a6910fbf098de6f6637cdeb
MengChi Chang <mengdesigntw@gmail.com>
2024-08-17 13:24:30 +0800

    add functionality when score reach 5 then game-set

--- app.js
+++ app.js
@@ -1,97 +1,133 @@
 console.log('in Js');
 
-// //Create a function that return random idx in array of choices.
-// function getComputerChoice() {
-//   const arr = ['Rock', 'Paper', 'Scissor'];
-//   const idx = Math.floor(Math.random() * arr.length);
-//   return arr[idx];
-// }
+//Create a function that return random idx in array of choices.
+function getComputerChoice() {
+  const arr = ['👊 Rock', '✋ Paper', '✌️ Scissor'];
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
 
-// //Create function that invoke a prompt for user to input choice, and return the input
-// function getHumanChoice() {
-//   const choice = prompt('Rock, Paper, or Scissor?', '');
-//   return choice;
-// }
+//Create function that invoke a prompt for user to input choice, and return the input
+function getHumanChoice() {
+  const choice = prompt('Rock, Paper, or Scissor?', '');
+  return choice;
+}
 
-// //create variable for human and computer, and initialize value with 0
-// let humanScore = 0;
-// let computerScore = 0;
+//create variable for human and computer, and initialize value with 0
+let humanScore = 0;
+let computerScore = 0;
 
-// //create a function to determine who wins the round.
-// function playRound(humanChoice, computerChoice) {
-//   let winningMsg = `You win! ${humanChoice} beats ${computerChoice}`;
-//   let losingMsg = `You lose! ${computerChoice} beats ${humanChoice}`;
-//   //check if a tie
-//   if (humanChoice === computerChoice) {
-//     return `It's a tie! You all go with ${humanChoice}`;
-//   }
-//   //then check the choice individually
-//   if (humanChoice === 'Rock') {
-//     if (computerChoice === 'Scissor') {
-//       humanScore += 1;
-//       return winningMsg;
-//     } else {
-//       computerScore += 1;
-//       return losingMsg;
-//     }
-//   }
-//   if (humanChoice === 'Scissor') {
-//     if (computerChoice === 'Paper') {
-//       humanScore += 1;
-//       return winningMsg;
-//     } else {
-//       computerScore += 1;
-//       return losingMsg;
-//     }
-//   }
-//   if (humanChoice === 'Paper') {
-//     if (computerChoice === 'Rock') {
-//       humanScore += 1;
-//       return winningMsg;
-//     } else {
-//       computerScore += 1;
-//       return losingMsg;
-//     }
-//   }
-// }
+//set event handler to each button and store value in variable
+const choices = document.querySelectorAll('#rock, #paper, #scissor');
 
-// //de-caseSensitive for the choice
-// function deCaseSensitive(text) {
-//   const modified = text.toLowerCase();
-//   const newText = modified.replace(modified[0], modified[0].toUpperCase());
-//   return newText;
-// }
+//create a function see if it's game-set
+function checkGameSet(){
+  if (humanScore == 5 || computerScore == 5) {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissor.disabled = true;
+  }
+}
 
-// //decide who wins the whole game
-// function isWinner() {
-//   if (humanScore !== computerScore) {
-//     return humanScore > computerScore ? 'You win!' : 'You lose!';
-//   } else {
-//     return `It's a tie!`;
-//   }
-// }
+//create a function to determine who wins the round.
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return '-';
+  }
+  if (
+    (humanChoice === 'Rock' && computerChoice === 'Scissor') ||
+    (humanChoice === 'Scissor' && computerChoice === 'Paper') ||
+    (humanChoice === 'Paper' && computerChoice === 'Rock')
+  ) {
+    humanScore += 1;
+    checkGameSet()
+    return 'You';
+  } else {
+    computerScore += 1;
+    checkGameSet()
+    return 'Computer';
+  }
+}
+
+//create variable to store score and display
+const computerScoreUi = document.querySelector('.score.computer');
+const humanScoreUi = document.querySelector('.score.human');
+computerScoreUi.textContent = computerScore;
+humanScoreUi.textContent = humanScore;
+
+const playHistory = document.querySelector('.play-history');
+//roundNum
+let i = 1;
+
+function handleClick(e) {
+  //get the value from computer and human
+  const humanValue = e.target.textContent; //👊 Rock
+  const humanValueModified = humanValue.slice(2).trimStart(); //Rock
+  const computerValue = getComputerChoice(); // e.g ✋ Papper
+  const computerValueModified = computerValue.slice(2).trimStart(); //e.g. Paper
+  //who is winner
+  const winner = playRound(humanValueModified, computerValueModified);
+  //inside play round
+  const playRoundUi = document.createElement('div');
+  playRoundUi.classList.toggle('play-round');
+  const roundData = document.createElement('div');
+  roundData.classList.toggle('round-data');
+  const roundWinner = document.createElement('div');
+  roundWinner.classList.toggle('round-winner');
+  playRoundUi.appendChild(roundData);
+  playRoundUi.appendChild(roundWinner);
+  //inside round data
+  const roundHeading = document.createElement('h5');
+  roundHeading.textContent = 'Round: ';
+  const playerChoice = document.createElement('div');
+  playerChoice.classList.toggle('player-choice');
+  roundData.appendChild(roundHeading);
+  roundData.appendChild(playerChoice);
+  //inside round heading
+  const roundNum = document.createElement('span');
+  roundNum.classList.toggle('round-num');
+  roundNum.textContent = i;
+  i++;
+  roundHeading.appendChild(roundNum);
+  //inside player choice
+  const humanChoiceUi = document.createElement('div');
+  humanChoiceUi.textContent = 'You: ';
+  const computerChoiceUi = document.createElement('div');
+  computerChoiceUi.textContent = 'Computer: ';
+  playerChoice.appendChild(humanChoiceUi);
+  playerChoice.appendChild(computerChoiceUi);
+  //inside human choice ui
+  const humanChoice = document.createElement('span');
+  humanChoice.textContent = humanValue; //'👊 Rock'
+  humanChoice.classList.toggle('emphasis');
+  humanChoiceUi.appendChild(humanChoice);
+  //inside computer choice ui
+  const computerChoice = document.createElement('span');
+  computerChoice.textContent = computerValue;
+  computerChoice.classList.toggle('emphasis');
+  computerChoiceUi.appendChild(computerChoice);
+  //inside round winner
+  const h5 = document.createElement('h5');
+  h5.textContent = 'Winner';
+  const winnerUi = document.createElement('div');
+  winnerUi.classList.toggle('emphasis');
+  winnerUi.textContent = winner; // computer, you, ''
+  roundWinner.appendChild(h5);
+  roundWinner.appendChild(winnerUi);
+  //append the play round inside the play history
+  playHistory.appendChild(playRoundUi);
+
+  //display score in the scoreboardUi
+  computerScoreUi.textContent = computerScore;
+  humanScoreUi.textContent = humanScore;
+}
+
+choices.forEach(choice => {
+  choice.addEventListener('click', handleClick);
+})
 
 // //reset the game
 // function reset() {
 //   humanScore = 0;
 //   computerScore = 0;
 // }
-
-// //create a function that will pay num round of games
-// function playGame(roundNum) {
-//   for (let i = 0; i < roundNum; i++) {
-//     //create variable to get deCaseSensitive choice
-//     const humanChoice = deCaseSensitive(getHumanChoice());
-//     const computerChoice = deCaseSensitive(getComputerChoice());
-//     const Msg = playRound(humanChoice, computerChoice);
-//     console.log(`Round ${i + 1}`);
-//     console.log(Msg);
-//     console.log(`Human:${humanScore}, Computer:${computerScore}`);
-//   }
-//   console.log('//// Game Set ////');
-//   console.log(isWinner());
-//   console.log(`Total run: ${roundNum}. Human:${humanScore}, Computer:${computerScore}.`);
-//   reset();
-// }
-
-// playGame(5);
